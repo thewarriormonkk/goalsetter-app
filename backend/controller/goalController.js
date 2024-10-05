e5760f6129d625bdbaf5e7cf521effa9146e45ee
Satendra Mishra <msatendra424@gmail.com>
2024-10-05 10:46:40 +0530

    error & exception handling

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,32 +1,42 @@
+const asyncHandler = require('express-async-handler');
+
 
 // @desc GET goals
 // @route GET /api/goals
 // @access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "get goals" })
-}
+})
 
 // @desc Set goal
 // @route POST /api/goals
 // @access Private
-const setGoal = (req, res) => {
-    res.status(301).json({ message: "goal created" });
-}
+const setGoal = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+        res.status(400);
+        throw new Error("Please add a text field");
+    }
+
+    res.status(201).json({ message: "goal created" });
+})
 
 // @desc Update goal
 // @route PUT /api/goals/:id
 // @access Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
+    console.log(req.params.id);
+
     res.status(200).json({ message: "goal updated" });
-}
+})
 
 // @desc Delete goal
 // @route DELETE /api/goals/:id
 // access Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
+    console.log(req.params.id);
 
     res.status(200).json({ message: "goal deleted" });
-}
+})
 
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 8000;
 
 const app = express()
 
 // middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
 app.use('/api/goals', require('./routes/goalRoutes'));
+
+// error handler
+app.use(errorHandler);
+
 
 app.listen(port, () => {
     console.log(`server listening on port ${port}`);
